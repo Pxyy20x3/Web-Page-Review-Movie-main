027ec51c40f4069a0d932863a1968e4288a2abd2
KittikarnIwai <kittigan1486@gmail.com>
2024-03-05 18:40:22 +0700

    Update Code

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const path = require("path");
 const axios = require("axios");
 const app = express();
 const port = process.env.PORT || 5500;
-// const cors = require("cors");
+const cors = require("cors");
 
 app.set("views", path.join(__dirname, "/public/views"));
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "/public")));
-// app.use(cors());
+app.use(cors());
 
 // Save Username
 app.use(
