115ac387dc865f23caed0ad157451ab867b15a93
KittikarnIwai <kittigan1486@gmail.com>
2024-03-05 18:02:32 +0700

    Update Code

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ app.use(
     })
 );
 
-const base_url = "http://localhost:3000";
-// const base_url = "http://node59842-moviemindshub.proen.app.ruk-com.cloud";
+// const base_url = "http://localhost:3000";
+const base_url = "http://node59929-moviemindsreviews.proen.app.ruk-com.cloud";
 
 // Home Routes
 app.get("/", (req, res) => {
